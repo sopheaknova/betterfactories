517e941b39f79ac6bea40628163dfcc30cd7f58e
sohpeaknova <novadesign.dev@gmail.com>
2013-01-30 17:23:50 +0700

    Search
    Fix search result page

--- searchform.php
+++ searchform.php
@@ -1,5 +1,4 @@
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label for="s" class="screenreader"><?php _e( 'Search', 'sptheme' ); ?></label>
-	<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search...', 'sptheme' ); ?>" />
-	<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'sptheme' ); ?>" />
-</form>
+        <input type="text" name="s" id="s" class="searchtxt" value="<?php _e( 'Search', 'sptheme' ); ?> &#8230;" onblur="if (this.value == '') {this.value = '<?php _e( 'Search', 'sptheme' ); ?> &#8230;';}" onfocus="if (this.value == '<?php _e( 'Search', 'sptheme' ); ?> &#8230;') {this.value = '';}">
+        <input type="submit" class="search-submit" value="<?php _e('Search', 'sptheme'); ?>" /><!-- hide this button in custom.js-->
+    </form>
