d9fe6e0cc7b55ca42efb0d79110609831d9e71b7
sohpeaknova <novadesign.dev@gmail.com>
2013-04-29 10:00:56 +0700

    Fixed: Vacancies page
    Render text for Contract type meta

--- content-job.php
+++ content-job.php
@@ -6,7 +6,7 @@
         <strong>Project title: </strong><?php echo sp_get_custom_field( 'sp_project_title', $post->ID ); ?><br />
         <strong>Post level: </strong><?php echo sp_get_custom_field( 'sp_post_level', $post->ID ); ?><br />
         <strong>Contract type: </strong><?php echo sp_get_custom_field( 'sp_contract_type', $post->ID ); ?><br />
-        <strong>Duration of contract: </strong><?php sp_get_custom_field( 'sp_contract_duration', $post->ID ); ?><br />
+        <strong>Duration of contract: </strong><?php echo sp_get_custom_field( 'sp_contract_duration', $post->ID ); ?><br />
         <strong>Duty station: </strong><?php echo sp_get_custom_field( 'sp_duty_station', $post->ID ); ?>
     </p>
     <?php the_content(); ?>
